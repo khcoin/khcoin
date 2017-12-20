432560d6d38595b40f560d7811617a5d1b0bc858
In Mean <inmean@gmail.com>
2017-12-20 16:30:07 +0700

    Release 0.15.0.2

--- src/validation.cpp
+++ src/validation.cpp
@@ -1032,6 +1032,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 	nSubsidy = 100 * COIN;
     } else if (nHeight == 33208) {
 	nSubsidy = 98400000 * COIN; // for distribute in Cambodia via ICO
+    } else if (nHeight > 45000) {
+	nSubsidy = 0; // for distribute in Cambodia via ICO
     }
 
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
